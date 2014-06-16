bc1dfe7ee132c313fafe601e234bfee38d39fe0c
Inder Dhir <inderdhir@hotmail.com>
2014-06-15 21:22:04 -0400

    implemented splash screen and a dummy login screen

--- src/com/example/gt_coc_studentevents/SplashScreen.java
+++ src/com/example/gt_coc_studentevents/SplashScreen.java
@@ -3,7 +3,9 @@ package com.example.gt_coc_studentevents;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,18 +13,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class SplashScreen extends ActionBarActivity {
+	
+	private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        new Handler().postDelayed(new Runnable() {
+        	 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);        
     }
 
 
